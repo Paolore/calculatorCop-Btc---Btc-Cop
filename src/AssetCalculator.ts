8f9df3b3b0d79c863e66b1723f30b5cd75d330c6
Lorena Correa <paolorena3@gmail.com>
2020-06-05 19:43:03 -0500

    Update AssetCalculator.ts

--- src/AssetCalculator.ts
+++ src/AssetCalculator.ts
@@ -1,19 +1,19 @@
 class AssetCalculator {
-  cryptoTRM: number;
-  decimals: number;
+  cryptoTRM: number
+  decimals: number
   constructor(cryptoTRM: number, decimals: number = 8) {
-    this.cryptoTRM = cryptoTRM;
-    this.decimals = decimals;
+    this.cryptoTRM = cryptoTRM
+    this.decimals = decimals
   }
 
   calculateFiat(crypto: number): number {
-    const fiatValue = crypto * this.cryptoTRM;
-    return Number(fiatValue);
+    const fiatValue = crypto * this.cryptoTRM
+    return Number(fiatValue)
   }
 
   calculateCrypto(fiat: number): number {
-    const cryptoValue = fiat / this.cryptoTRM ;
-    return Number(cryptoValue.toFixed(this.decimals));
+    const cryptoValue = fiat / this.cryptoTRM 
+    return Number(cryptoValue.toFixed(this.decimals))
   }
 }
 
